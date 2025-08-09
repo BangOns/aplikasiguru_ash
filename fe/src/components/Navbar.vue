bbc36feed5cd108068d257a0bf59875fc61ca4fc
bangOns <syahr9950@gmail.com>
2025-08-09 10:24:26 +0700

    feat: Create project

---
+++ fe/src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="w-full flex justify-center items-center p-4 text-white">
+    <div class="container w-full flex justify-between items-center">
+      <section class="flex items-center">
+        <h1 class="font-richis text-2xl">Arisan Yuks</h1>
+      </section>
+    </div>
+  </header>
+</template>
