7148715e5ced2be996900af919a617aaaa9b7494
bangOns <syahr9950@gmail.com>
2025-08-09 12:31:16 +0700

    feat: Create Header

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import Navbar from "./components/Navbar.vue";
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <main class="w-full min-h-screen bg-primary text-white">
-    <Navbar />
-    <router-view />
-  </main>
+  <Header />
+  <router-view />
 </template>
---
+++ fe/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header
+    class="w-full flex justify-center items-center relative text-white h-72 bg-primary"
+  >
+    <div class="flex justify-between items-end w-full h-full absolute bottom-0">
+      <div class="w-full h-36 overflow-hidden">
+        <div
+          class="w-full h-full bg-amber-300 shadow-2xl shadow-black inset-shadow-black mount"
+        ></div>
+      </div>
+
+      <div class="w-full h-36 overflow-hidden">
+        <div
+          class="w-full h-full bg-amber-300 shadow-2xl shadow-black inset-shadow-black mount2"
+        ></div>
+      </div>
+    </div>
+
+    <div class="container w-full flex justify-between items-center"></div>
+  </header>
+</template>
